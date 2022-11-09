28df9a4db40addaf1a58c0edc07f80384392e099
YaroslavChuiko <jarik.tchuicko@gmail.com>
2022-11-09 21:46:19 +0200

    add COLOR_PATTERN

--- consts/validation.js
+++ consts/validation.js
@@ -2,6 +2,7 @@ const NAME_LENGTH = { min: 4, max: 20 };
 const PASSWORD_LENGTH = { min: 8, max: 20 };
 const CALENDAR_NAME_LENGTH = { min: 5, max: 100 };
 const CALENDAR_DESCRIPTION_LENGTH = { min: 10, max: 100 };
+const COLOR_PATTERN = /^#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/;
 
 const ROLE_ENUM = {
   admin: 'admin',
@@ -9,4 +10,11 @@ const ROLE_ENUM = {
   moderator: 'moderator',
 };
 
-module.exports = { NAME_LENGTH, PASSWORD_LENGTH, ROLE_ENUM, CALENDAR_NAME_LENGTH, CALENDAR_DESCRIPTION_LENGTH };
+module.exports = {
+  NAME_LENGTH,
+  PASSWORD_LENGTH,
+  ROLE_ENUM,
+  CALENDAR_NAME_LENGTH,
+  CALENDAR_DESCRIPTION_LENGTH,
+  COLOR_PATTERN,
+};
