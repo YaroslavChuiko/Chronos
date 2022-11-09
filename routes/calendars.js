01e9a7c682670c3c62a50ce467d90493f8c5265e
YaroslavChuiko <jarik.tchuicko@gmail.com>
2022-11-09 21:48:11 +0200

    add PUT /calendar/:id and DELETE /calendar/:id endpoints

--- routes/calendars.js
+++ routes/calendars.js
@@ -1,15 +1,19 @@
 const express = require('express');
-const { createCalendar } = require('~/controllers/calendars');
+const { createCalendar, updateCalendar, deleteCalendar } = require('~/controllers/calendars');
 const authenticate = require('~/middleware/auth');
 const boundary = require('~/helpers/error-boundary');
 const validate = require('~/helpers/validation');
-const { createSchema } = require('~/validation/calendar');
+const { createSchema, updateSchema } = require('~/validation/calendar');
 
 const router = express.Router();
 
-router.post('/', authenticate, validate(createSchema), boundary(createCalendar));
-// router.put('/:id', )
-// router.delete('/:id', )
-// router.post('/:id/events/:id', )
+router.use(authenticate);
+
+router.post('/', validate(createSchema), boundary(createCalendar));
+router.put('/:id', validate(updateSchema), boundary(updateCalendar));
+router.delete('/:id', boundary(deleteCalendar));
+
+// router.post('/:calendarId/events/:eventId');
+// router.delete('/:calendarId/events/:eventId');
 
 module.exports = router;
