de020d680329cb5d62a5c49b6f23968f59c848de
YaroslavChuiko <jarik.tchuicko@gmail.com>
2022-11-10 18:23:14 +0200

    add EVENT_TYPE_ENUM,  replace ROLE_ENUM to ROLE

--- consts/validation.js
+++ consts/validation.js
@@ -4,16 +4,29 @@ const CALENDAR_NAME_LENGTH = { min: 5, max: 100 };
 const CALENDAR_DESCRIPTION_LENGTH = { min: 10, max: 100 };
 const COLOR_PATTERN = /^#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/;
 
-const ROLE_ENUM = {
-  admin: 'admin',
-  guest: 'guest',
-  moderator: 'moderator',
+const ROLE = {
+  calendar: {
+    admin: 'admin',
+    guest: 'guest',
+    moderator: 'moderator',
+  },
+  event: {
+    admin: 'admin',
+    guest: 'guest',
+  },
+};
+
+const EVENT_TYPE_ENUM = {
+  arrangement: 'arrangement',
+  reminder: 'reminder',
+  task: 'task',
 };
 
 module.exports = {
   NAME_LENGTH,
   PASSWORD_LENGTH,
-  ROLE_ENUM,
+  ROLE,
+  EVENT_TYPE_ENUM,
   CALENDAR_NAME_LENGTH,
   CALENDAR_DESCRIPTION_LENGTH,
   COLOR_PATTERN,
