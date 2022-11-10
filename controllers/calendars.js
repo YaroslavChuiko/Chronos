4a3dbe0e0b2467a86b4bee1fdb1e49d5389e811e
YaroslavChuiko <jarik.tchuicko@gmail.com>
2022-11-10 22:59:51 +0200

    add deleteCalendarEvent

--- controllers/calendars.js
+++ controllers/calendars.js
@@ -1,7 +1,7 @@
 const templates = require('~/consts/email');
 const { ROLE } = require('~/consts/validation');
 const ServerError = require('~/helpers/server-error');
-const { calendar, user, userCalendars, event } = require('~/lib/prisma');
+const { calendar, user, userCalendars, event, userEvents, calendarEvents } = require('~/lib/prisma');
 const { Factory, Email, User, Token } = require('~/services');
 
 const authorCheck = async (req, _res, next) => {
@@ -72,7 +72,18 @@ const deleteCalendar = async (req, res) => {
 const createCalendarEvent = async (req, res) => {
   const data = req.body;
   const calendarId = Number(req.params.id);
-  const { id } = req.user;
+  const { id: userId } = req.user;
+  const { admin, moderator } = ROLE.calendar;
+
+  const exist = await calendar.findUnique({ where: { id: calendarId } });
+  if (!exist) {
+    throw new ServerError(404, 'The calendar does not exist.');
+  }
+
+  const toCheck = await userCalendars.findUnique({ where: { userId_calendarId: { userId, calendarId } } });
+  if (!toCheck || (toCheck.role !== admin && toCheck.role !== moderator)) {
+    throw new ServerError(403, 'You do not have the rights to perform this action.');
+  }
 
   const newEvent = await event.create({
     data: {
@@ -80,7 +91,7 @@ const createCalendarEvent = async (req, res) => {
       users: {
         create: {
           role: ROLE.event.admin,
-          user: { connect: { id } },
+          user: { connect: { id: userId } },
         },
       },
       calendars: {
@@ -92,6 +103,42 @@ const createCalendarEvent = async (req, res) => {
   });
 
   res.status(201).json(newEvent);
+};
+
+const deleteCalendarEvent = async (req, res) => {
+  const calendarId = Number(req.params.calendarId);
+  const eventId = Number(req.params.eventId);
+  const { id: userId } = req.user;
+
+  //! check is calendar exist
+  //! check role in calendar
+
+  const exist = await event.findUnique({ where: { id: eventId } });
+  if (!exist) {
+    throw new ServerError(404, 'The event does not exist.');
+  }
+
+  const toCheck = await userEvents.findUnique({ where: { userId_eventId: { userId, eventId } } });
+  if (!toCheck) {
+    throw new ServerError(403, 'You do not have the rights to perform this action.');
+  }
+
+  if (toCheck.role === ROLE.event.admin) {
+    await event.delete({ where: { id: eventId } });
+  }
+
+  if (toCheck.role === ROLE.event.guest) {
+    Promise.all([
+      calendarEvents.delete({
+        where: { calendarId_eventId: { calendarId, eventId } },
+      }),
+      userEvents.delete({
+        where: { userId_eventId: { userId, eventId } },
+      }),
+    ]);
+  }
+
+  res.sendStatus(204);
 };
 
 const getInvitedUsers = async (req, res) => {
@@ -166,6 +213,7 @@ module.exports = {
   updateCalendar,
   deleteCalendar,
   createCalendarEvent,
+  deleteCalendarEvent,
   shareCalendar,
   confirmCalendar,
   getInvitedUsers,
