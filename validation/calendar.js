41cc1671b370cd4c4be5f21023d0cb7066cfdd15
YaroslavChuiko <jarik.tchuicko@gmail.com>
2022-11-10 18:24:03 +0200

    add createEventSchema

--- validation/calendar.js
+++ validation/calendar.js
@@ -1,5 +1,10 @@
 const Joi = require('joi');
-const { CALENDAR_NAME_LENGTH, CALENDAR_DESCRIPTION_LENGTH, COLOR_PATTERN } = require('~/consts/validation');
+const {
+  CALENDAR_NAME_LENGTH,
+  CALENDAR_DESCRIPTION_LENGTH,
+  COLOR_PATTERN,
+  EVENT_TYPE_ENUM,
+} = require('~/consts/validation');
 
 const createSchema = Joi.object().keys({
   name: Joi.string().required().min(CALENDAR_NAME_LENGTH.min).max(CALENDAR_NAME_LENGTH.max),
@@ -13,8 +18,20 @@ const updateSchema = Joi.object().keys({
   color: Joi.string().required().pattern(COLOR_PATTERN, 'color'),
 });
 
+//! mb move to event.js
+const createEventSchema = Joi.object().keys({
+  name: Joi.string().required().min(CALENDAR_NAME_LENGTH.min).max(CALENDAR_NAME_LENGTH.max), //! change min, max
+  content: Joi.string().required().min(CALENDAR_DESCRIPTION_LENGTH.min).max(CALENDAR_DESCRIPTION_LENGTH.max), //! change min, max
+  color: Joi.string().required().pattern(COLOR_PATTERN, 'color'),
+  type: Joi.string()
+    .required()
+    .valid(...Object.values(EVENT_TYPE_ENUM)),
+  startAt: Joi.date().required().iso().less(Joi.ref('endAt')).min('now'), //? type iso or timestamp('javascript')
+  endAt: Joi.date().required().iso(),
+});
+
 const shareSchema = Joi.object().keys({
   email: Joi.string().email().required(),
 });
 
-module.exports = { createSchema, shareSchema, updateSchema };
+module.exports = { createSchema, shareSchema, updateSchema, createEventSchema };
