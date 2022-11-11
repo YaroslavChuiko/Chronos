f3849cf9045ecf79f9769831f9e2596dd65adc2f
YaroslavChuiko <jarik.tchuicko@gmail.com>
2022-11-11 13:27:20 +0200

    add PATCH /calendars/:id/events/:eventid endpoint, refactor

--- routes/event.js
+++ routes/event.js
@@ -1,13 +1,14 @@
 const express = require('express');
-const { createCalendarEvent, deleteCalendarEvent } = require('~/controllers/calendars');
+const { updateEvent, createEvent, deleteEvent } = require('~/controllers/events');
 const boundary = require('~/helpers/error-boundary');
 const validate = require('~/helpers/validation');
-const { createEventSchema } = require('~/validation/calendar');
+const { updateSchema, createSchema } = require('~/validation/event');
 
 const router = express.Router({ mergeParams: true });
 
-router.post('/', validate(createEventSchema), boundary(createCalendarEvent));
+router.post('/', validate(createSchema), boundary(createEvent));
 
-router.delete('/:eventId', boundary(deleteCalendarEvent));
+router.patch('/:eventId', validate(updateSchema), boundary(updateEvent));
+router.delete('/:eventId', boundary(deleteEvent));
 
 module.exports = router;
