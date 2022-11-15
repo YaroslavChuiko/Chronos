44dca69c28a679defa0dfdf64b8cfb80a0158ed9
Polina Balazhy <lefugdot@gmail.com>
2022-11-15 12:28:27 +0200

    add calendar filtering by id

--- api/controllers/calendars.js
+++ api/controllers/calendars.js
@@ -10,6 +10,12 @@ const { Factory, Email, Token } = require('~/services');
 
 const getCalendars = async (req, res) => {
   const { id } = req.user;
+  const { calendarIDs: ids } = req.body;
+
+  const filters = {};
+  if (ids && ids.length) {
+    filters.id = { in: ids };
+  }
 
   const calendars = await Factory.findMany(
     calendar,
@@ -17,6 +23,7 @@ const getCalendars = async (req, res) => {
       users: {
         some: { user: { id } },
       },
+      ...filters,
     },
     null,
   );
--- api/validation/calendar.js
+++ api/validation/calendar.js
@@ -1,6 +1,10 @@
 const Joi = require('joi');
 const { CALENDAR_NAME_LENGTH, CALENDAR_DESCRIPTION_LENGTH, COLOR_PATTERN } = require('~/consts/validation');
 
+const filterSchema = Joi.object().keys({
+  calendarIDs: Joi.array().items(Joi.number()),
+});
+
 const createSchema = Joi.object().keys({
   name: Joi.string().required().min(CALENDAR_NAME_LENGTH.min).max(CALENDAR_NAME_LENGTH.max),
   description: Joi.string().min(CALENDAR_DESCRIPTION_LENGTH.min).max(CALENDAR_DESCRIPTION_LENGTH.max),
@@ -17,4 +21,4 @@ const shareSchema = Joi.object().keys({
   email: Joi.string().email().required(),
 });
 
-module.exports = { createSchema, shareSchema, updateSchema };
+module.exports = { filterSchema, createSchema, shareSchema, updateSchema };
