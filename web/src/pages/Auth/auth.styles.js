46e4fe03c32b64fa077f2a1ec66bcf72c4940e9c
YaroslavChuiko <jarik.tchuicko@gmail.com>
2022-11-17 22:43:24 +0200

    create styles

---
+++ web/src/pages/Auth/auth.styles.js
@@ -0,0 +1,36 @@
+const styles = {
+  wrapper: {
+    background: 'gray.100',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100vh',
+    padding: '10px',
+  },
+  container: {
+    background: 'white',
+    padding: '40px 40px 30px',
+    borderRadius: '10px',
+    maxWidth: '380px',
+    width: '100%',
+  },
+  heading: {
+    fontSize: '20px',
+    marginBottom: '35px',
+    textAlign: 'center',
+  },
+  footer: {
+    marginTop: '20px',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  footerText: {
+    color: 'gray.500',
+    fontSize: 'sm',
+  },
+  footerLink: {
+    color: 'teal.500',
+    marginLeft: '5px',
+  },
+};
+
+export default styles;
