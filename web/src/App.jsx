d2388d3940b89e3754ca2c14c4a01781821dad5a
YaroslavChuiko <jarik.tchuicko@gmail.com>
2022-11-17 22:45:38 +0200

    add /register route

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,14 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 import Hello from '~/pages/Hello/Hello';
-import Login from './pages/Login/Login';
+import Login from './pages/Auth/Login';
+import Register from './pages/Auth/Register';
 
 const App = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Hello />} />
-        <Route path="/test" element={<div>Test</div>} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </>
   );
