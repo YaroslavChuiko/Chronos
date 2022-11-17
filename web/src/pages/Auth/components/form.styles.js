8686fdae856271acd7d1d6ae68b645f7d4d90caa
YaroslavChuiko <jarik.tchuicko@gmail.com>
2022-11-17 22:44:15 +0200

    create styles

---
+++ web/src/pages/Auth/components/form.styles.js
@@ -0,0 +1,14 @@
+const styles = {
+  vStack: {
+    alignItems: 'flex-start',
+  },
+  formLabel: {
+    fontSize: 'sm',
+    marginLeft: '5px',
+  },
+  button: {
+    width: '100%',
+  },
+};
+
+export default styles;
