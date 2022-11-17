e1f75fead5460d43ff589ea6297f321a0f91453d
YaroslavChuiko <jarik.tchuicko@gmail.com>
2022-11-17 13:46:19 +0200

    move login endpoint to authSlice, add baseQueryWithReauth

--- web/src/store/api/apiSlice.js
+++ web/src/store/api/apiSlice.js
@@ -1,28 +1,14 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { setToken, setUser } from '../authSlice';
+import { createApi } from '@reduxjs/toolkit/query/react';
+import baseQueryWithReauth from './baseQueryWithReauth';
 
 export const apiSlice = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_API_URL }),
+  baseQuery: baseQueryWithReauth,
   endpoints: (builder) => ({
     getCalendars: builder.query({
       query: () => `/calendars`,
     }),
-    login: builder.mutation({
-      query: ({ login, password }) => ({
-        url: 'auth/login',
-        method: 'POST',
-        body: { login, password },
-      }),
-      async onQueryStarted(_args, { dispatch, queryFulfilled }) {
-        try {
-          const { data } = await queryFulfilled;
-          dispatch(setUser(data));
-          dispatch(setToken(data));
-        } catch (error) {}
-      },
-    }),
   }),
 });
 
-export const { useGetCalendarsQuery, useLoginMutation } = apiSlice;
+export const { useGetCalendarsQuery } = apiSlice;
