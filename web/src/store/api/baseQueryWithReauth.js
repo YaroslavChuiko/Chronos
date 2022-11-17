86a965373c64c810a18cf88cecb868d06afb8aae
YaroslavChuiko <jarik.tchuicko@gmail.com>
2022-11-17 13:46:36 +0200

    create baseQueryWithReauth

---
+++ web/src/store/api/baseQueryWithReauth.js
@@ -0,0 +1,41 @@
+import { fetchBaseQuery } from '@reduxjs/toolkit/dist/query';
+import { logout, setCredentials } from '../authSlice';
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: process.env.REACT_APP_API_URL,
+  prepareHeaders: (headers, { getState }) => {
+    const token = getState().auth.accessToken;
+    if (token) {
+      headers.set('authorization', `Bearer ${token}`);
+    }
+    return headers;
+  },
+  credentials: 'include',
+});
+
+const baseQueryWithReauth = async (args, api, extraOptions) => {
+  let result = await baseQuery(args, api, extraOptions);
+
+  if (result.error && result.error.status === 401) {
+    // try to get a new token
+    const refreshResult = await baseQuery(
+      {
+        url: 'auth/refresh',
+        method: 'POST',
+      },
+      api,
+      extraOptions
+    );
+    if (refreshResult.data) {
+      // store the new token
+      api.dispatch(setCredentials(refreshResult.data));
+      // retry the initial query
+      result = await baseQuery(args, api, extraOptions);
+    } else {
+      api.dispatch(logout());
+    }
+  }
+  return result;
+};
+
+export default baseQueryWithReauth;
