fd0759dfefebe1b32383cadb7259a78a6eb526fe
YaroslavChuiko <jarik.tchuicko@gmail.com>
2022-11-18 20:28:06 +0200

    add title

--- web/src/pages/NotFound/NotFound.jsx
+++ web/src/pages/NotFound/NotFound.jsx
@@ -1,5 +1,17 @@
+import { Button } from '@chakra-ui/react';
+import { Link as RouterLink } from 'react-router-dom';
 import PageAlert from '~/components/PageAlert/PageAlert';
 
-const NotFound = () => <PageAlert status="error" message="This page does not exist."></PageAlert>;
+const NotFound = () => (
+  <PageAlert
+    status="warning"
+    title="404 - Page not found!"
+    message="We're sorry, the page you requested could not be found. Please go back to the homepage."
+  >
+    <Button colorScheme="orange" as={RouterLink} to="/">
+      Go home
+    </Button>
+  </PageAlert>
+);
 
 export default NotFound;
