5630693053cfd1895af97059d3cdfe3553c2e4e0
Polina Balazhy <lefugdot@gmail.com>
2022-11-18 11:51:14 +0200

    Add the not found page

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -3,6 +3,7 @@ import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Register";
 import CalendarPage from "./pages/Main/Main";
 import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
+import NotFound from "./pages/NotFound/NotFound";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         </Route>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </>
   );
---
+++ web/src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,7 @@
+import PageAlert from "~/components/PageAlert/PageAlert";
+
+const NotFound = () => (
+  <PageAlert status="error" message="This page does not exist."></PageAlert>
+);
+
+export default NotFound;
