cb9fc6b5e996a61298fa1bc67fb068c6545fd196
YaroslavChuiko <jarik.tchuicko@gmail.com>
2022-11-21 19:48:45 +0200

    add EVENT_NAME_LENGTH, COLOR_PATTERN, EVENT_TYPE_ENUM

--- web/src/consts/validation.js
+++ web/src/consts/validation.js
@@ -1,4 +1,12 @@
 const LOGIN_LENGTH = { min: 4, max: 20 };
 const PASSWORD_LENGTH = { min: 8, max: 20 };
+const EVENT_NAME_LENGTH = { min: 5, max: 20 };
+const COLOR_PATTERN = /^#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/;
 
-export { LOGIN_LENGTH, PASSWORD_LENGTH };
+const EVENT_TYPE_ENUM = {
+  arrangement: 'arrangement',
+  reminder: 'reminder',
+  task: 'task',
+};
+
+export { LOGIN_LENGTH, PASSWORD_LENGTH, EVENT_NAME_LENGTH, COLOR_PATTERN, EVENT_TYPE_ENUM };
