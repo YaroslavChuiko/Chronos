5f44030721083a629e9d7ce53ab9ee55c2f8639f
YaroslavChuiko <jarik.tchuicko@gmail.com>
2022-11-22 20:57:52 +0200

    add createCalendarEvent endpoint

--- web/src/store/api/apiSlice.js
+++ web/src/store/api/apiSlice.js
@@ -1,12 +1,20 @@
 import { createApi } from '@reduxjs/toolkit/query/react';
+import moment from 'moment';
 import baseQueryWithReauth from './baseQueryWithReauth';
 
 export const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: baseQueryWithReauth,
+  tagTypes: ['Event'],
   endpoints: (builder) => ({
     getCalendars: builder.query({
-      query: () => `/calendars`,
+      query: ({ roles } = { roles: [] }) => ({
+        url: `/calendars`,
+        params: {
+          ...(roles.length && { roles: roles.join(',') }),
+        },
+      }),
+      providesTags: ['Calendar'],
     }),
     getHolidays: builder.query({
       query: () => `/calendars/holidays`,
@@ -19,9 +27,32 @@ export const apiSlice = createApi({
           ...(types.length && { types: types.join(',') }),
         },
       }),
+      providesTags: (result, _error, _arg) => {
+        const events = result || [];
+        return ['Event', ...events.map(({ id }) => ({ type: 'Event', id }))];
+      },
+    }),
+    createCalendarEvent: builder.mutation({
+      query: ({ calendar, name, content, color, start, end, type }) => ({
+        url: `/calendars/${calendar}/events`,
+        method: 'POST',
+        body: {
+          name,
+          content,
+          color,
+          start: moment(start).format(),
+          end: moment(end).format(),
+          type,
+        },
+      }),
+      invalidatesTags: ['Event'],
     }),
   }),
 });
 
-export const { useGetCalendarsQuery, useLazyGetHolidaysQuery, useGetCalendarEventsQuery } =
-  apiSlice;
+export const {
+  useGetCalendarsQuery,
+  useLazyGetHolidaysQuery,
+  useGetCalendarEventsQuery,
+  useCreateCalendarEventMutation,
+} = apiSlice;
