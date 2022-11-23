5691e0394dee288bc3bc37168c520ef3fb8a5727
YaroslavChuiko <jarik.tchuicko@gmail.com>
2022-11-23 21:49:31 +0200

    create

---
+++ web/src/pages/CreateEvent/createEvent.styles.js
@@ -0,0 +1,27 @@
+const styles = {
+  wrapper: {
+    background: 'gray.100',
+    minHeight: '100vh',
+    height: '100%',
+    padding: '20px',
+  },
+  header: {
+    marginBottom: '40px',
+  },
+  heading: {
+    marginLeft: '30px',
+  },
+  content: {
+    width: '100%',
+    justifyContent: 'center',
+  },
+  container: {
+    background: 'white',
+    padding: '30px 20px 20px',
+    borderRadius: '10px',
+    maxWidth: '800px',
+    width: '100%',
+  },
+};
+
+export default styles;
