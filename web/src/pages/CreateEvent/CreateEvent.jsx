d2ae176e64d52946492fbfeca9ae7f6da920e70f
YaroslavChuiko <jarik.tchuicko@gmail.com>
2022-11-23 19:31:19 +0200

    update imports

--- web/src/pages/CreateEvent/CreateEvent.jsx
+++ web/src/pages/CreateEvent/CreateEvent.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Layout from '~/components/Layout/Layout';
-import CreateEventTabs from '~/components/Modal/CreateEventTabs';
+import CreateEventTabs from '~/components/Tabs/CreateEventTabs';
 
 const CreateEvent = () => {
   return (
