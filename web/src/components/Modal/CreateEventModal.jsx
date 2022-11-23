63ef7821bded53e883cb38dc62a1b80bf92dfeac
YaroslavChuiko <jarik.tchuicko@gmail.com>
2022-11-23 19:04:55 +0200

    update

--- web/src/components/Modal/CreateEventModal.jsx
+++ web/src/components/Modal/CreateEventModal.jsx
@@ -5,28 +5,10 @@ import {
   ModalContent,
   ModalHeader,
   ModalOverlay,
-  Tab,
-  TabList,
-  TabPanel,
-  TabPanels,
-  Tabs,
 } from '@chakra-ui/react';
-import { useEffect, useState } from 'react';
-import { useGetCalendarsQuery } from '~/store/api/apiSlice';
-import ArrangementForm from './ArrangementForm';
-import TaskForm from './TaskForm';
+import CreateEventTabs from './CreateEventTabs';
 
 const CreateEventModal = ({ isOpen, onClose, selectedDate }) => {
-  const [userCalendars, setUserCalendars] = useState([]);
-
-  const { data } = useGetCalendarsQuery({ roles: ['admin', 'moderator'] });
-
-  useEffect(() => {
-    if (data) {
-      setUserCalendars(data);
-    }
-  }, [data]);
-
   return (
     <Modal isOpen={isOpen} onClose={onClose} isCentered>
       <ModalOverlay />
@@ -34,33 +16,7 @@ const CreateEventModal = ({ isOpen, onClose, selectedDate }) => {
         <ModalHeader>Create an event</ModalHeader>
         <ModalCloseButton />
         <ModalBody>
-          <Tabs>
-            <TabList>
-              <Tab>Arrangement</Tab>
-              <Tab>Task</Tab>
-              <Tab>Reminder</Tab>
-            </TabList>
-
-            <TabPanels>
-              <TabPanel>
-                <ArrangementForm
-                  onClose={onClose}
-                  initialDate={selectedDate}
-                  userCalrndars={userCalendars}
-                />
-              </TabPanel>
-              <TabPanel>
-                <TaskForm
-                  onClose={onClose}
-                  initialDate={selectedDate}
-                  userCalrndars={userCalendars}
-                />
-              </TabPanel>
-              <TabPanel>
-                <p>three!</p>
-              </TabPanel>
-            </TabPanels>
-          </Tabs>
+          <CreateEventTabs onClose={onClose} selectedDate={selectedDate} />
         </ModalBody>
       </ModalContent>
     </Modal>
