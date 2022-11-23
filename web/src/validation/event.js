08be71635d499b993bc06cae53897d58e159367a
YaroslavChuiko <jarik.tchuicko@gmail.com>
2022-11-23 19:07:40 +0200

    fix

--- web/src/validation/event.js
+++ web/src/validation/event.js
@@ -2,7 +2,7 @@ import * as Yup from 'yup';
 import { COLOR_PATTERN, EVENT_NAME_LENGTH, EVENT_TYPE_ENUM } from '~/consts/validation';
 
 export const arrangementSchema = Yup.object().shape({
-  calendar: Yup.string().required('Calendar is requierd!'),
+  calendar: Yup.string().required('Calendar is required!'),
   name: Yup.string().required().min(EVENT_NAME_LENGTH.min).max(EVENT_NAME_LENGTH.max),
   content: Yup.string(),
   color: Yup.string().matches(COLOR_PATTERN),
@@ -12,7 +12,7 @@ export const arrangementSchema = Yup.object().shape({
 });
 
 export const taskSchema = Yup.object().shape({
-  calendar: Yup.string().required('Calendar is requierd!'),
+  calendar: Yup.string().required('Calendar is required!'),
   name: Yup.string().required().min(EVENT_NAME_LENGTH.min).max(EVENT_NAME_LENGTH.max),
   content: Yup.string(),
   color: Yup.string().matches(COLOR_PATTERN),
@@ -21,7 +21,7 @@ export const taskSchema = Yup.object().shape({
 });
 
 export const reminderSchema = Yup.object().shape({
-  calendar: Yup.string().required('Calendar is requierd!'),
+  calendar: Yup.string().required('Calendar is required!'),
   name: Yup.string().required().min(EVENT_NAME_LENGTH.min).max(EVENT_NAME_LENGTH.max),
   content: Yup.string(),
   color: Yup.string().matches(COLOR_PATTERN),
