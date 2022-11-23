3bb7d10501bb5dbc738d1571e47e0e931cf79440
YaroslavChuiko <jarik.tchuicko@gmail.com>
2022-11-23 21:49:20 +0200

    add styles

--- web/src/pages/CreateEvent/CreateEvent.jsx
+++ web/src/pages/CreateEvent/CreateEvent.jsx
@@ -1,12 +1,34 @@
-import React from 'react';
-import Layout from '~/components/Layout/Layout';
+import { ArrowBackIcon } from '@chakra-ui/icons';
+import { Box, Button, Flex, Heading } from '@chakra-ui/react';
+import { Link as RouterLink } from 'react-router-dom';
 import CreateEventTabs from '~/components/Tabs/CreateEventTabs';
+import styles from './createEvent.styles';
 
 const CreateEvent = () => {
   return (
-    <Layout>
-      <CreateEventTabs />
-    </Layout>
+    <Box sx={styles.wrapper}>
+      <Flex sx={styles.header}>
+        <Button
+          leftIcon={<ArrowBackIcon boxSize={5} />}
+          as={RouterLink}
+          to=".."
+          colorScheme="green"
+          variant="ghost"
+          size="lg"
+        >
+          Back
+        </Button>
+
+        <Heading as="h1" size="lg" sx={styles.heading}>
+          Create an event
+        </Heading>
+      </Flex>
+      <Flex sx={styles.content}>
+        <Box sx={styles.container}>
+          <CreateEventTabs />
+        </Box>
+      </Flex>
+    </Box>
   );
 };
 
