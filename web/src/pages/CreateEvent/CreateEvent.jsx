addcd6993f0dda417e21b0e52747c49ad2e63edf
YaroslavChuiko <jarik.tchuicko@gmail.com>
2022-11-23 19:05:14 +0200

    create CreateEvent component

---
+++ web/src/pages/CreateEvent/CreateEvent.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '~/components/Layout/Layout';
+import CreateEventTabs from '~/components/Modal/CreateEventTabs';
+
+const CreateEvent = () => {
+  return (
+    <Layout>
+      <CreateEventTabs />
+    </Layout>
+  );
+};
+
+export default CreateEvent;
