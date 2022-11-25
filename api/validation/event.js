8614b9e15a10bfa0c3c6f3fd44ac656d85e33344
YaroslavChuiko <jarik.tchuicko@gmail.com>
2022-11-25 16:43:19 +0200

    update updateSchema

--- api/validation/event.js
+++ api/validation/event.js
@@ -1,10 +1,5 @@
 const Joi = require('joi');
-const {
-  COLOR_PATTERN,
-  EVENT_TYPE_ENUM,
-  CALENDAR_DESCRIPTION_LENGTH,
-  EVENT_NAME_LENGTH,
-} = require('~/consts/validation');
+const { COLOR_PATTERN, EVENT_TYPE_ENUM, EVENT_NAME_LENGTH } = require('~/consts/validation');
 
 const createSchema = Joi.object().keys({
   name: Joi.string().required().min(EVENT_NAME_LENGTH.min).max(EVENT_NAME_LENGTH.max),
@@ -18,10 +13,10 @@ const createSchema = Joi.object().keys({
 });
 
 const updateSchema = Joi.object().keys({
-  name: Joi.string().required().min(EVENT_NAME_LENGTH.min).max(EVENT_NAME_LENGTH.max), //! change min, max
-  content: Joi.string().required().min(CALENDAR_DESCRIPTION_LENGTH.min).max(CALENDAR_DESCRIPTION_LENGTH.max), //! change min, max
+  name: Joi.string().required().min(EVENT_NAME_LENGTH.min).max(EVENT_NAME_LENGTH.max),
+  content: Joi.string().allow(''),
   color: Joi.string().pattern(COLOR_PATTERN, 'color'),
-  start: Joi.date().required().iso().less(Joi.ref('end')).min('now'), //? type iso or timestamp('javascript')
+  start: Joi.date().required().iso().less(Joi.ref('end')),
   end: Joi.date().required().iso(),
 });
 
